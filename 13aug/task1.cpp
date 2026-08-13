cdc5fa46dc93f53c48936dc61467e0bc63913007
eshakhaliqdev <mkhaliqwarraich123@gmail.com>
2026-08-13 16:35:57 +0500

    first task done of 13 aug

---
+++ 13aug/task1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Ali" << endl;
+    cout << "City: Lahore" << endl;
+    cout << "Subject: Computer Science" << endl;
+    return 0;
+}
