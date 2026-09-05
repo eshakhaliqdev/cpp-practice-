d7cd3c8da303b615f19d67f429dbf191b35d3e23
eshakhaliqdev <mkhaliqwarraich123@gmail.com>
2026-09-05 20:21:56 +0500

    Add basic C++ introduction program

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My name is Esha khaliq"<<endl;
+    cout<<"I live in Gujrat"<<endl;
+    cout<<"I am 19 years old"<<endl;
+    return 0;
+}
