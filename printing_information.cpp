21d7583ea2436eef49572590383a97cca8c1b997
eshakhaliqdev <mkhaliqwarraich123@gmail.com>
2026-09-09 11:23:05 +0500

    Added printing information program

---
+++ printing_information.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"My name is Esha Khaliq"<<endl;
+    cout<<"I am 19 years old"<<endl;
+    cout<<"My favourite Language is c++"<<endl;
+    return 0;
+}
