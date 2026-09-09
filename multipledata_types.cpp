1cbaa800b1ea25a579905df840b96f28f7cb7c54
eshakhaliqdev <mkhaliqwarraich123@gmail.com>
2026-09-09 12:51:00 +0500

    add multiple data types prgram

---
+++ multipledata_types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    /*This program write by a university student.
+The purpose of this program to gives detail of Gpa Record.git*/
+    int Age = 20;
+    float GPA = 3.6;
+    char Grade = 'A';
+    cout<<"Age = "<< Age <<endl;
+    cout<<"GPA = "<< GPA <<endl;
+    cout<<"Grade = "<< Grade <<endl;
+    return 0;
+
+}
